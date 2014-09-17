ce8ecb52b2910dc04b8dc8394c1ae219a0533737
navneet <user@Navneet-MBP.local>
2014-09-17 16:46:44 +0530

    initial rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notesapp::Application.config.secret_token = 'd6a596150c13d8b34b9733f78d08c25dc9f912829fd39137ca2630894b4de5f850c286a8bb9352541ca7c7c4795dd1d4cb5370f8e3beb739b866adb4bea037b5'
